34a21a5febe6c6230ba5830730ce16e9e3ab6270
Alisher <roziqovalisher177@gmail.com>
2025-04-08 19:28:57 +0500

    ds

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,5 +1,3 @@
-import { createElement } from 'react'
-
-export default function ProductById({ params } :{params:{id:number}}) {
+export default function ProductById({ params } : { params: { id: string } }) {
 	return <div className='text-2xl text-center mt-5'>{params.id}</div>
 }
